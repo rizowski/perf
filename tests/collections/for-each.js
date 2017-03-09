f439b44794c901486f06e2698c5141df4b980baf
Colten Rouska <colten.rouska@vivintsolar.com>
2017-03-09 13:19:44 -0700

    Adding underscore into the mix

--- tests/collections/for-each.js
+++ tests/collections/for-each.js
@@ -1,6 +1,7 @@
 const _ = require('lodash');
 const Benchmark = require('benchmark');
 const R = require('ramda');
+const under = require('underscore');
 const suite = new Benchmark.Suite;
 
 const largerList = [];
@@ -12,11 +13,14 @@ function sub1(element){
   return element - 1;
 }
 
-module.exports = suite.add('_.forEach', () =>{
+module.exports = suite.add('lodash.forEach', () =>{
   _.forEach(largerList, sub1);
 })
-.add('R.forEach', () =>{
+.add('ramda.forEach', () =>{
   R.forEach(sub1, largerList);
+})
+.add('underscore.forEach', () =>{
+  under.each(largerList, sub1);
 })
 .add('[].forEach', () =>{
   largerList.forEach(sub1);
--- tests/collections/map.js
+++ tests/collections/map.js
@@ -1,6 +1,7 @@
 const _ = require('lodash');
 const Benchmark = require('benchmark');
 const R = require('ramda');
+const under = require('underscore');
 const suite = new Benchmark.Suite;
 
 const largerList = [];
@@ -12,14 +13,17 @@ function add1(element){
   return element + 1;
 }
 
-module.exports = suite.add('_.map', () =>{
+module.exports = suite.add('lodash.map', () =>{
   _.map(largerList, add1);
 })
-.add('R.map', () =>{
-  R.map(add1, largerList);
-})
-.add('_(arr).map', () =>{
+.add('lodash(arr).map', () =>{
   _.chain(largerList).map(add1).value()
+})
+.add('underscore.map', () =>{
+  under.map(largerList, add1);
+})
+.add('ramda.map', () =>{
+  R.map(add1, largerList);
 })
 .add('[].map', () =>{
   largerList.map(add1);
