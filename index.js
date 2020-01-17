1ae011a683302ed74423aa6bf437fd1139f650fc
Colten Rouska <crouska@gmail.com>
2020-01-17 11:05:51 -0700

    backing up work

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ let report = require('./reporting');
 let tests = underscore.flatten([
   // require('./perf-tests/collections'),
   // require('./perf-tests/objects'),
-  // require('./perf-tests/naturals/new-promise')
+  require('./perf-tests/naturals/new-promise'),
   // require('./perf-tests/naturals/conditionals')
-  require('./perf-tests/naturals/operators')
+  // require('./perf-tests/naturals/operators')
 ]);
 
-underscore.forEach(tests, t =>{
+underscore.forEach(tests, t => {
   t.on('complete', function() {
     report.runReports(this);
   })
-  .on('error', (e) =>{
-    console.error(e.target.error.stack)
-    process.exit(1);
-  })
-  .run({ 'async': process.env.ASYNC || false });
-})
+    .on('error', e => {
+      console.error(e.target.error.stack);
+      process.exit(1);
+    })
+    .run({ async: process.env.ASYNC || false });
+});
